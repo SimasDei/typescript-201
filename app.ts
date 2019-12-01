91f4e1820220cf626d7a7c1b652f230bbe49d7ca
SimasDei <simondeiman@gmail.com>
2019-12-01 17:23:09 +0200

    testing core types 1/?

--- app.ts
+++ app.ts
@@ -1,1 +1,14 @@
-console.log('Your code goes here...');
+const number1: number = 5;
+const number2: number = 2.9;
+const printResult = true;
+const passedPhrase = 'Ahoy sailor o/';
+
+const add = ((
+  n1: number = number1,
+  n2: number = number2,
+  showResult: boolean = printResult,
+  phrase: string = passedPhrase,
+) => {
+  if (!showResult) console.log(n1 + n2);
+  else return `${phrase}: ${n1 + n2}`;
+})();
